2737af68ca99c28ecb3505d4108aa87c453692c5
Yaroslav Gurov <ygurov@proton.me>
2025-08-10 16:59:41 +0200

    fix: (win) better IPv6 handling and router fixes

--- service/server/router_win.h
+++ service/server/router_win.h
@@ -7,12 +7,12 @@
 #include <QHash>
 #include <QDebug>
 #include <QObject>
+#include <QNetworkInterface>
 
 #include "../client/platforms/windows/daemon/dnsutilswindows.h"
 
 #include <WinSock2.h>  //includes Windows.h
 #include <WS2tcpip.h>
-
 
 #include <iphlpapi.h>
 #include <IcmpAPI.h>
@@ -49,6 +49,8 @@ public:
     bool updateResolvers(const QString& ifname, const QList<QHostAddress>& resolvers);
 
 private:
+    static QList<QString> kIpv6Subnets;
+
     RouterWin(RouterWin const &) = delete;
     RouterWin& operator= (RouterWin const&) = delete;
 
@@ -57,6 +59,8 @@ private:
     BOOL EnableDebugPrivilege();
     BOOL InitNtFunctions();
     BOOL SuspendProcess(BOOL fSuspend, DWORD dwProcessId);
+
+    QNetworkInterface findLoopbackIface();
 
 private:
     RouterWin() {m_dnsUtil = new DnsUtilsWindows(this);}
